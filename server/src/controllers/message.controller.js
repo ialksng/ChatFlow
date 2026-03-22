9f66ad8211cba90a40f96f4f9053007eb58495c9
Alok Singh <ialksng@gmail.com>
2026-03-23 05:13:05 +0530

    Update message.controller.js

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -31,3 +31,32 @@ export const getMessages = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   };
 };
+
+export const sendMessages = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      // Upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // Realtime functionality -> Socket.io
+
+  } catch (error) {
+
+  }
+};
