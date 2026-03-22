0593169db94fd1da74ae8bda19feedb94bdd4267
Alok Singh <ialksng@gmail.com>
2026-03-23 01:18:46 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes)
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
