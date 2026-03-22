e7be4206e14c0a7efa5e3b49a52fce49d7d9781c
Alok Singh <ialksng@gmail.com>
2026-03-23 04:13:57 +0530

    Create cloudinary.js

---
+++ server/src/lib/cloudinary.js
@@ -0,0 +1,13 @@
+import { v2 as cloudinary } from "cloudinary";
+
+import { config } from 'dotenv';
+
+config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    cloud_key: process.env.CLOUDINARY_API_KEY,
+    cloud_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;
