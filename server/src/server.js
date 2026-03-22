50f79ce6ed1cf1ceb73f6e57ca462bd588778b8d
Alok Singh <ialksng@gmail.com>
2026-03-23 01:40:13 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js"
 
 dotenv.config();
 const app = express();
@@ -12,4 +14,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    connectDB();
 });
