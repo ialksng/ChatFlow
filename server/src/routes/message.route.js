71e0f20e697981ad29c67ef33b2db38b353b1be6
Alok Singh <ialksng@gmail.com>
2026-03-23 05:06:59 +0530

    Update message.route.js

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar, getMessages } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
+
+router.send("/send/:id", protectRoute, sendMessages);
 
 export default router;
