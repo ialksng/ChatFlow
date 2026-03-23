328a19f6aa3cfdeffcd0729ab2e717bb0bb9212f
Alok Singh <ialksng@gmail.com>
2026-03-23 19:19:21 +0530

    Update message.controller.js

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -30,10 +32,10 @@ export const getMessages = async (req, res) => {
   } catch (error) {
     console.log("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
-  };
+  }
 };
 
-export const sendMessages = async (req, res) => {
+export const sendMessage = async (req, res) => {
   try {
     const { text, image } = req.body;
     const { id: receiverId } = req.params;
@@ -55,9 +57,14 @@ export const sendMessages = async (req, res) => {
 
     await newMessage.save();
 
-    // Realtime functionality -> Socket.io
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
+    res.status(201).json(newMessage);
   } catch (error) {
-
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
