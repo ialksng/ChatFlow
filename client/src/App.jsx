264f7363cf72cb02241f753ce3c0c4dbb2db8ae6
Alok Singh <ialksng@gmail.com>
2026-03-23 06:05:25 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,11 @@ const App = () => {
       <Navbar />
 
       <Routes>
-
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
 
     </div>
