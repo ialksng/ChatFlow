2309032c9f44f1e7c783b8ca4b14402d9e520061
Alok Singh <ialksng@gmail.com>
2026-03-23 06:57:10 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,17 @@ import ProfilePage from "./pages/ProfilePage";
 
 import { Routes, Route } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
 const App = () => {
-  const { authUser } = useAuthStore(); 
+  const { authUser, checkAuth } = useAuthStore(); 
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({authUser});
+
   return (
     <div>
 
