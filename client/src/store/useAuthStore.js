fa968faf454de32d6b648907bef50823c5cd4154
Alok Singh <ialksng@gmail.com>
2026-03-23 13:45:15 +0530

    Update useAuthStore.js

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import { axiosInstance } from "../lib/axios.js"
 export const useAuthStore = create((set) => ({
     authUser: null,
     isSigningUp: false,
-    isLogging: false,
+    isLoggingIn: false,
     isUpdatingProfile: false,
 
     isCheckingAuth: true,
