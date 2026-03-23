75c01dcd5b99ff24915a2f5bb66dd7f93e81a429
Alok Singh <ialksng@gmail.com>
2026-03-23 15:03:07 +0530

    Update useAuthStore.js

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -22,5 +22,17 @@ export const useAuthStore = create((set) => ({
         }
     },
 
-    signup: async (data) => {},
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+        const res = await axiosInstance.post("/auth/signup", data);
+        set({ authUser: res.data });
+        toast.success("Account created successfully");
+        get().connectSocket();
+        } catch (error) {
+        toast.error(error.response.data.message);
+        } finally {
+        set({ isSigningUp: false });
+        }
+    },
 }));
