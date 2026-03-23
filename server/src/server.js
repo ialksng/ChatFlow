99b34f8971d0ec3b09cf562b75062c14eb18fb4a
Alok Singh <ialksng@gmail.com>
2026-03-23 19:04:31 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -3,15 +3,18 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,8 +23,16 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+};
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
