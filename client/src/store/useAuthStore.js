5ee35c5464aea4ce71471e6bd5575599eaefba86
Alok Singh <ialksng@gmail.com>
2026-03-24 23:00:46 +0530

    redirect

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "production" ? "http://localhost:8080" : "https://chatflow-zvul.onrender.com";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8080" : "https://chatflow-zvul.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
