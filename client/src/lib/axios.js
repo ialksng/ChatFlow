2ca1dc5824aaea387836f361724e839cfe504ed1
Alok Singh <ialksng@gmail.com>
2026-03-24 22:49:37 +0530

    redirect

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.MODE === "production" ? "http://localhost:8080/api" : "https://chatflow-zvul.onrender.com/api",
+    baseURL: import.meta.env.MODE === "production" ? "http://localhost:8080/api" : "https://chatflow-zvul.onrender.com/api",
     withCredentials: true,
 });
