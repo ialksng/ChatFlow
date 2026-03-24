00c696b8290add4b9a640b48f138109ac0fb1dc5
Alok Singh <ialksng@gmail.com>
2026-03-24 20:00:03 +0530

    Update message.model.js

--- server/src/models/message.model.js
+++ server/src/models/message.model.js
@@ -1,26 +1,47 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-    {
-        senderId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        receiverId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        text: {
-            type: String,
-        },
-        image: {
-            type: String,
-        },
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {timestamps: true}
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    isEdited: {
+      type: Boolean,
+      default: false,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false, 
+    },
+    replyTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
+    reactions: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        emoji: { type: String },
+      }
+    ],
+  },
+  { timestamps: true }
 );
+
+messageSchema.index({ text: "text" });
 
 const Message = mongoose.model("Message", messageSchema);
 
