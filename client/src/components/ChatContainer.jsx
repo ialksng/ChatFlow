afc8205358b533e01bea69b0114be4edeeb3e589
Alok Singh <ialksng@gmail.com>
2026-03-25 01:34:13 +0530

    profile  opic

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -121,7 +121,7 @@ const ChatContainer = () => {
               <div className="chat-image avatar">
                 <div className="size-10 rounded-full border">
                   <img
-                    src={isMine ? authUser.profilePic || "/avatar.png" : selectedUser.profilePic || "/avatar.png"}
+                    src={isMine ? authUser.profilePic || "avatar.png" : selectedUser.profilePic || "/avatar.png"}
                     alt="profile pic"
                   />
                 </div>
@@ -266,7 +266,10 @@ const ChatContainer = () => {
           <div className="chat chat-start">
             <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
-                <img src={selectedUser.profilePic || "/avatar.png"} alt="profile pic" />
+                  <img
+                    src={isMine ? authUser.profilePic || "avatar.png" : selectedUser.profilePic || "/avatar.png"}
+                    alt="profile pic"
+                  />
               </div>
             </div>
             <div className="chat-header mb-1">
